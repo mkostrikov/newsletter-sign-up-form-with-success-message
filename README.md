83736b7d297497589fe0aa506645ea2e8f71510d
Mikhail Kostrikov <kostrikovma@vk.com>
2024-04-30 23:22:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Newsletter sign-up form with success message
+
+## The challenge
+
+Users should be able to:
+
+- Add their email and submit the form
+- See a success message with their email after successfully submitting the form
+- See form validation messages if:
+  - The field is left empty
+  - The email address is not formatted correctly
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Links
+
+- Live Site URL: [live site URL here](https://mkostrikov.github.io/newsletter-sign-up-form-with-success-message/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
---
+++ src/assets/js/main.js
@@ -0,0 +1,37 @@
+const subscribeForm = document.forms["subscribe-form"];
+const emailInput = subscribeForm.email;
+const errorMessage = document.querySelector("#email-validation");
+
+subscribeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const subscribeInner = document.querySelector("#subscribe-inner");
+
+    if (!emailInput.checkValidity()) {
+        emailInvalid();
+    } else {
+        subscribeInner.classList.add("subscribe__rotate");
+        document.querySelector("#dismiss-message").addEventListener("click", () => {
+            subscribeInner.classList.remove("subscribe__rotate");
+        }, {once: true});
+        emailInput.value = "";
+    }
+});
+
+emailInput.addEventListener("blur", (e) => {
+    if (!e.target.checkValidity()) {
+        emailInvalid();
+    } else {
+        emailValid();
+    }
+});
+
+function emailInvalid() {
+    errorMessage.textContent = "Valid email required";
+    emailInput.classList.add("form__input--invalid");
+}
+
+function emailValid() {
+    errorMessage.textContent = "";
+    emailInput.classList.remove("form__input--invalid");
+}
